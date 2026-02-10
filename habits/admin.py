d8834bea88ccedd3e6f0c895b1229fc9af855944
Mohameds Lap <elmhlawy89@gmail.com>
2026-02-10 21:13:08 +0200

    first commit

---
+++ habits/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import Habits
+
+admin.site.register(Habits)
---
+++ habits/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+class Habits(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    date_created = models.DateField(timezone.now)
+    
